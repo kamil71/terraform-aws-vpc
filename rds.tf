1b2a1115d8ffbf7cd786e561019febfc813fa01e
Kamil Pathan <pkamil717@gmail.com>
2022-03-13 18:33:36 +0530

    Initial Commit

--- rds.tf
+++ rds.tf
@@ -10,9 +10,35 @@ username = "admin"
 password = "Admin54132"
 parameter_group_name = "default.mysql5.7"
 db_subnet_group_name = aws_db_subnet_group.db_subnet.id
+publicly_accessible = true
+vpc_security_group_ids   = ["${aws_security_group.mydb1.id}"]
 }
 
 resource "aws_db_subnet_group" "db_subnet" {
 name = "db subnet group"
-subnet_ids = aws_subnet.private_subnet.*.id
+subnet_ids = aws_subnet.public_subnet.*.id
+#  subnet_ids = aws_subnet.public_subnet..id
+}
+
+resource "aws_security_group" "mydb1" {
+  name = "mydb1"
+
+  description = "RDS postgres servers (terraform-managed)"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  # Only postgres in
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic.
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
